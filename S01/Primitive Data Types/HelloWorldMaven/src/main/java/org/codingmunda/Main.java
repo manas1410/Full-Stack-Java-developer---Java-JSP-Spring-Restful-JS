e2f6e4474402ef0d0e929c1cd8c6cf399ab22e74
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-09 00:26:18 +0530

    Primitive Data Types

---
+++ S01/Primitive Data Types/HelloWorldMaven/src/main/java/org/codingmunda/Main.java
@@ -0,0 +1,19 @@
+package org.codingmunda;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int var1 = 1;
+        float var2 = 2.0f;
+        double var3 = 3.0;
+        double var4 = 3.0d;
+        long var5 = 6;
+        boolean var6 = true;
+        char var7 = 's';
+        char var8 = '\u00A7';
+        System.out.println(var8);
+        System.out.printf("Hello and welcome!");
+
+    }
+}
