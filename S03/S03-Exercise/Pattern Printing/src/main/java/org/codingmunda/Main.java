67b21390719bf6ecf3458cd77392cbcd10397434
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-09 23:46:18 +0530

    Pattern Printing

---
+++ S03/S03-Exercise/Pattern Printing/src/main/java/org/codingmunda/Main.java
@@ -0,0 +1,16 @@
+package org.codingmunda;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // (a+b)^2 = a^2 + 2ab + b^2
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+
+    }
+}
